0592eef0ca506a7c983b0b55f862da1ed0ce1946
Lucas Lelieur <lucas.lelieur.ll@gmail.com>
2024-11-21 15:12:28 +0100

    charts created

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,13 +5,8 @@ import MoviesGlobalFilter from "../../components/MoviesGlobalFilter/MoviesGlobal
 import { useEffect, useState } from "react";
 import { Container, Button, Col, Row } from 'react-bootstrap/'
 
-import { MdVolumeOff, MdVolumeUp } from "react-icons/md"
-import Century from '../../assets/music/Century.mp3'
-
 const HomePage = () => {
 
-    const [audio, setAudio] = useState()
-    const [isMuted, setIsMuted] = useState(true)
     const [filterSelected, setFilterSelected] = useState("")
 
     const handleFilterSelected = filter => {
@@ -19,28 +14,17 @@ const HomePage = () => {
         setFilterSelected(filter)
     }
 
-    useEffect(() => {
-        const audioInstance = new Audio(Century)
-        setAudio(audioInstance)
-        audioInstance.play().catch((error) => {
-            console.log(error)
-        })
-        return () => {
-            audioInstance.pause()
-            audioInstance.currentTime = 0
-        }
-    }, [])
-
-    const handleMute = () => {
-        audio.muted = !isMuted
-        setIsMuted(!isMuted)
-    }
-
     return (
-        <div className="HomePage h-100 bg-black text-white">
+        <div className="HomePage text-white">
 
             <Container className="d-flex justify-content-center align-items-center min-vh-100">
-                <div className="w-70 text-center">
+
+                <video autoPlay loop muted playsInline className="video-content">
+                    <source src="https://res.cloudinary.com/dhluctrie/video/upload/v1732182659/background-video_cudoag.mp4" type="video/mp4" />
+                    Tu navegador no soporta videos.
+                </video>
+
+                <div className="w-70 text-center" style={{ zIndex: 100 }}>
                     <h1 className="text-decoration-underline">LA PREMIERE</h1>
                     <h2>Encuentra tu peli favorita, en tu cine favorito</h2>
                     <Row className="mt-5">
@@ -63,14 +47,6 @@ const HomePage = () => {
 
 
             </Container>
-
-            <div className="d-none position-sticky top-0">
-                <span
-                    onClick={handleMute}
-                    style={{ fontSize: "2rem", cursor: "pointer" }}>
-                    {isMuted ? <MdVolumeOff /> : <MdVolumeUp />}
-                </span>
-            </div>
 
         </div >
     )
---
+++ src/pages/Stats/MovieStatsPage.jsx
@@ -0,0 +1,113 @@
+import { useEffect, useState } from "react"
+
+import { Row, Col, Container } from "react-bootstrap"
+
+import axios from "axios"
+import Loader from "../../components/Loader/Loader"
+
+const API_URL = "http://localhost:5005"
+
+import MoviesGenrePieChart from "../../components/MoviesGenrePieChart/MoviesGenrePieChart"
+import CinemasSeatingPieChart from "../../components/CinemasSeatingPieChart/CinemasSeatingPieChart"
+
+import "./MovieStatsPage.css"
+
+const MovieStatsPage = () => {
+
+    const [isLoading, setIsLoading] = useState(true)
+    const [cinemas, setCinemas] = useState([])
+    const [movies, setMovies] = useState([])
+
+    let numLongMovies = 0
+    let numShortMovies = 0
+
+    movies.forEach(eachMovie => {
+        if (!eachMovie.isDeleted && eachMovie.duration > 120) {
+            numLongMovies++
+        } else if (!eachMovie.isDeleted && eachMovie.duration <= 120) {
+            numShortMovies++
+        }
+    })
+
+    const longFilmsData = {
+        id: "> 120 min",
+        value: numLongMovies
+    }
+
+    const shortFilmsData = {
+        id: "< 120 min",
+        value: numShortMovies
+    }
+
+    const durationData = [longFilmsData, shortFilmsData]
+
+
+    let smallCinemas = 0
+    let bigCinemas = 0
+
+    cinemas.forEach(eachCinema => {
+        if (!eachCinema.isDeleted && Number(eachCinema.capacity.seating) <= 600) {
+            smallCinemas++
+        } else if (!eachCinema.isDeleted && Number(eachCinema.capacity.seating) > 600) {
+            bigCinemas++
+        }
+    })
+
+    const bigCinemasData = {
+        id: "> 600 butacas",
+        value: bigCinemas
+    }
+
+    const smallCinemasData = {
+        id: "< 600 butacas",
+        value: smallCinemas
+    }
+
+    const seatingData = [bigCinemasData, smallCinemasData]
+
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+
+
+    const fetchData = () => {
+
+        const promises = [
+            axios.get(`${API_URL}/movies/`),
+            axios.get(`${API_URL}/cinemas/`),
+        ]
+
+        Promise
+            .all(promises)
+            .then(([movies, cinemas]) => {
+                setMovies(movies.data)
+                setCinemas(cinemas.data)
+            })
+            .then(() => setIsLoading(false))
+            .catch(err => console.log(err))
+    }
+
+
+    return (
+
+        isLoading ? <Loader /> :
+
+            <div className="MovieStatsPage">
+                <Container className="h-100">
+                    <Row className="h-100 d-flex justify-content-center align-items-center">
+                        <Col md={6} className="text-center">
+                            <MoviesGenrePieChart data={durationData} />
+                        </Col>
+                        <Col md={6} className="text-center">
+                            <CinemasSeatingPieChart data={seatingData} />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+
+    )
+}
+
+export default MovieStatsPage
